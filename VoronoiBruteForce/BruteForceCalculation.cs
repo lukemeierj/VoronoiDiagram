5e969a18d8553f64186080ecce266557abcf406a
Noah Allen <noahtallen@gmail.com>
2018-11-01 13:38:55 -0400

    Sketch implementation for brute force

---
+++ VoronoiBruteForce/BruteForceCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoronoiBruteForce
+{
+    public class BruteForceCalculation
+    {
+        readonly List<Point> sites;
+        readonly Double width;
+        readonly Double height;
+
+        public BruteForceCalculation(List<Point> sites, Double width, Double height)
+        {
+            this.sites = sites;
+            this.width = width;
+            this.height = height;
+        }
+
+        public List<Cell> CalculateVoronoiDiagram() {
+            return null;
+        }
+    }
+}
--- VoronoiBruteForce/Program.cs
+++ VoronoiBruteForce/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VoronoiBruteForce
 {
+    public struct Point {
+        readonly Double x, y;
+        public Point(Double x, Double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public struct Edge {
+        readonly Point start, end;
+        public Edge(Point start, Point end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public struct Cell {
+        readonly Point site;
+        public List<Edge> boundary;
+        public Cell(Point site, List<Edge> boundary) {
+            this.site = site;
+            this.boundary = boundary;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point A = new Point(5, 5);
+            Point B = new Point(10, 10);
+            Point C = new Point(60, 70);
+            List<Point> testSitesA = new List<Point>(){ A, B, C };
+
+            BruteForceCalculation algoTest1 = new BruteForceCalculation(testSitesA, 100, 100);
+            List<Cell> res1 = algoTest1.CalculateVoronoiDiagram();
+            PrintResults(res1);
+        }
+
+        public static void PrintResults(List<Cell> cells) {
+            if (cells != null) {
+                Console.WriteLine("There are " + cells.Count + " cells.");
+            } else {
+                Console.WriteLine("No results.");
+            }
         }
     }
 }
