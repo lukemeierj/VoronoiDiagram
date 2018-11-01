e1f295f4a853c09b04507eeb29380697254194c3
Noah Allen <noahtallen@gmail.com>
2018-11-01 15:03:39 -0400

    more work

--- VoronoiBruteForce/BruteForceCalculation.cs
+++ VoronoiBruteForce/BruteForceCalculation.cs
@@ -1,23 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
 
 namespace VoronoiBruteForce
 {
     public class BruteForceCalculation
     {
         readonly List<Point> sites;
-        readonly Double width;
-        readonly Double height;
+        readonly int width;
+        readonly int height;
 
-        public BruteForceCalculation(List<Point> sites, Double width, Double height)
+        // Note: we will go through the width/height from 0 up to (excluding) the value for width/height
+        public BruteForceCalculation(List<Point> sites, int width, int height)
         {
             this.sites = sites;
             this.width = width;
             this.height = height;
         }
 
-        public List<Cell> CalculateVoronoiDiagram() {
-            return null;
+        // Some information was gathered from this StackOverflow answer:
+        // https://stackoverflow.com/a/85484
+        public Color[,] CalculateVoronoiDiagram() {
+            Color[,] output = new Color[height, width];
+
+            // row goes through each row from top to bottom
+            for (int row = 0; row < height; row++) {
+                // col goes through each col from right to left
+                for (int col = 0; col < width; col++) {
+                    // col = x, since col tracks rtl
+                    // row = y, since row tracks up/down
+                    Point curPoint = new Point(col, row);
+
+                    // Arbitrarily start at the first site:
+                    Point closestSite = sites[0];
+                    double closestDistance = DistanceBetweenPoints(curPoint, closestSite);
+
+                    // Start after the first site:
+                    foreach (Point site in sites.Skip(1)) {
+                        double distance = DistanceBetweenPoints(curPoint, site);
+                        // Better than the previous point:
+                        if (distance < closestDistance) {
+                            closestSite = site;
+                            closestDistance = distance;
+                        }
+                    }
+                    output[row, col] = closestSite.color;
+                }
+            }
+            return output;
+        }
+
+        public static double DistanceBetweenPoints (Point p1, Point p2) {
+            double xVal = Math.Pow((p2.x - p1.x), 2);
+            double yVal = Math.Pow((p2.y - p1.y), 2);
+            return Math.Sqrt(xVal + yVal);
         }
     }
 }
--- VoronoiBruteForce/Program.cs
+++ VoronoiBruteForce/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace VoronoiBruteForce
 {
-    public struct Point {
-        readonly Double x, y;
-        public Point(Double x, Double y) {
+    public struct Point
+    {
+        public double x, y;
+        public Color color;
+        public Point(double x, double y)
+        {
             this.x = x;
             this.y = y;
+        }
+        public Point(double x, double y, Color c) {
+            this.x = x;
+            this.y = y;
+            color = c;
         }
     }
 
@@ -33,14 +43,27 @@ namespace VoronoiBruteForce
     {
         static void Main(string[] args)
         {
-            Point A = new Point(5, 5);
-            Point B = new Point(10, 10);
-            Point C = new Point(60, 70);
+            Point A = new Point(5, 5, Color.Aqua);
+            Point B = new Point(10, 10, Color.Beige);
+            Point C = new Point(60, 70, Color.SeaGreen);
+            int width = 100;
+            int height = 100;
             List<Point> testSitesA = new List<Point>(){ A, B, C };
 
-            BruteForceCalculation algoTest1 = new BruteForceCalculation(testSitesA, 100, 100);
-            List<Cell> res1 = algoTest1.CalculateVoronoiDiagram();
-            PrintResults(res1);
+            BruteForceCalculation algoTest1 = new BruteForceCalculation(testSitesA, width, height);
+            Color[,] res1 = algoTest1.CalculateVoronoiDiagram();
+            SaveAsImage(res1, width, height);
+        }
+
+        public static void SaveAsImage (Color[,] voronoi, int width, int height) {
+            Bitmap image = new Bitmap(width, height, PixelFormat.Canonical);
+
+            for (int row = 0; row < width; row++) {
+                for (int col = 0; col < height; col++) {
+                    image.SetPixel(col, row, voronoi[col, row]);
+                }
+            }
+            image.Save("img.bmp");
         }
 
         public static void PrintResults(List<Cell> cells) {
